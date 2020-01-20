67e00471b98fd69270166cd85416bc6ade651d7c
Pop Adriana <anni.ania19@yahoo.com>
2020-01-20 13:23:49 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/Product.java
@@ -0,0 +1,113 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String description;
+    @NotNull
+    private String name;
+    @NotNull
+    private Double price;
+    @NotNull
+    private Integer quantity;
+    private String imageUrl;
+
+    // "products" is the name of the property in Cart where we defined the relationship
+    @ManyToMany(mappedBy = "products")
+    private Set<Cart> carts = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Set<Cart> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(Set<Cart> carts) {
+        this.carts = carts;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return id.equals(product.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
---
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -0,0 +1,63 @@
+package org.fasttrackit.onlineshop.web;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.GetProductsRequest;
+import org.fasttrackit.onlineshop.transfer.ProductResponse;
+import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // todo: fix lazy loading for all endpoints
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid SaveProductRequest request) {
+        Product product = productService.createProduct(request);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
+        Product product = productService.getProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getProducts(
+            GetProductsRequest request, Pageable pageable) {
+        Page<ProductResponse> products = productService.getProducts(request, pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable("id") Long id, @RequestBody @Valid SaveProductRequest request) {
+        Product product = productService.updateProduct(id, request);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+//    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
